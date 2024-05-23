f7834fb19a82ef2eefe57229c0873bb99448cbc8
bhusandas <sonubhusan98@gmail.com>
2024-05-23 15:26:05 +0530

    Added city model and migration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const {PORT} = require('./config/serverconfig');
 const setupAndStartServer = async() =>{
     const app = express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
     app.listen(PORT,() => {
         console.log(`The server is running on port ${PORT}`);
         //console.log(process.env); 
