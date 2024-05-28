69c1b4f19d41d965105b3381337966daf29a24c4
bhusandas <sonubhusan98@gmail.com>
2024-05-28 16:53:59 +0530

    Added Airplane model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyparser = require('body-parser');
 const {PORT} = require('./config/serverconfig');
 //const CityRepository = require('./repository/city-repository');
 const ApiRoutes = require('./routes/index');
+const {Airport,City,Airplane} = require('./models/index')
+const db = require('./models/index');
 
 const setupAndStartServer = async() =>{
     const app = express();
@@ -11,11 +13,31 @@ const setupAndStartServer = async() =>{
     app.use('/api',ApiRoutes);
     app.listen(PORT,async () => {
         console.log(`The server is running on port ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+        }
+        // const airports =  await Airport.findAll({
+        //     include: [{
+        //         model: City,
+        //     }]
+        // });
+        // console.log(airports);
         
         //console.log(process.env);
         // const repo = new CityRepository();
         // repo.createCity({name:"new delhi"});
-       
+        const city = await City.findOne({
+            where: {
+                id: 7
+            }
+        });
+        const airports = await city.getAirports()
+        console.log(city,airports);
+        await Airplane.create({
+            modelNumber: "Boeing 878",
+            createdAt : new Date(),
+            updatedAt: new Date()
+        })
     })
 }
 
